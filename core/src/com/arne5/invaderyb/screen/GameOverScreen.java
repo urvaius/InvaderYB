fc11523252e6968954dc0a02866fcf1d46226aa6
Dave Arneson <urvaius@gmail.com>
2016-05-25 15:30:38 -0700

    working now and all updates seems good.

---
+++ core/src/com/arne5/invaderyb/screen/GameOverScreen.java
@@ -0,0 +1,119 @@
+package com.arne5.invaderyb.screen;
+
+import com.arne5.invaderyb.Assets;
+import com.arne5.invaderyb.Entity.EntityManager;
+import com.arne5.invaderyb.InvaderYB;
+import com.arne5.invaderyb.camera.OrthoCamera;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.math.Vector2;
+
+
+/**
+ * Created by urvaius on 9/21/14.
+ */
+public class GameOverScreen extends Screen
+	{
+		private OrthoCamera camera;
+		private Sprite sprite;
+		private Sprite restartButton;
+		private BitmapFont highScoreFont;
+		private GlyphLayout layout;
+
+
+
+		public GameOverScreen(boolean won)
+			{
+				if(won)
+					{
+						sprite = Assets.gameWon;
+					}
+				else
+					{
+						sprite = Assets.gameOver;
+					}
+			}
+
+
+		@Override
+		public void create()
+			{
+				camera= new OrthoCamera();
+				camera.resize();
+
+				highScoreFont = new BitmapFont();
+				highScoreFont.setColor(Color.WHITE);
+				layout = new GlyphLayout();
+				restartButton = Assets.restartButton;
+
+
+				Gdx.app.getPreferences("GamePref").flush();
+
+			}
+
+		@Override
+		public void update()
+
+			{
+				camera.update();
+
+
+				if(Gdx.input.isTouched())
+					{
+						Vector2 touch = camera.unprojectCoordinates(Gdx.input.getX(), Gdx.input.getY());
+						if (touch.x < 0 + Assets.restartButton.getWidth() && touch.y < 0 + Assets.restartButton.getHeight())
+							{
+								ScreenManager.setScreen(new GameScreen());
+							}
+
+					}
+			}
+
+
+
+
+
+
+		@Override
+		public void render(SpriteBatch sb)
+			{
+				sb.setProjectionMatrix(camera.combined);
+				sb.begin();
+
+				sb.draw(sprite, InvaderYB.WIDTH / 2 - sprite.getWidth() / 2, InvaderYB.HEIGHT / 2 - sprite.getHeight() / 2);
+				layout.setText(highScoreFont, "HighScore "+ Gdx.app.getPreferences("GamePref").getInteger("HighScore"));
+
+				highScoreFont.draw(sb,layout, 10 , InvaderYB.HEIGHT - highScoreFont.getXHeight() -layout.height*2 );
+				sb.draw(restartButton,0,0);
+				sb.end();
+			}
+
+		@Override
+		public void resize(int width, int height)
+			{
+				camera.resize();
+			}
+
+		@Override
+		public void dispose()
+			{
+
+			}
+
+		@Override
+		public void pause()
+			{
+
+			}
+
+		@Override
+		public void resume()
+			{
+
+			}
+	}
--- desktop/src/com/arne5/invaderyb/desktop/DesktopLauncher.java
+++ desktop/src/com/arne5/invaderyb/desktop/DesktopLauncher.java
@@ -3,10 +3,19 @@ package com.arne5.invaderyb.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.arne5.invaderyb.InvaderYB;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
+
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+		TexturePacker.process(settings, "C:/Development/github/invaderyb/spaceimages","C:/Development/github/invaderyb/android/assets","pack");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title="Invader YB";
+		config.width=InvaderYB.WIDTH;
+		config.height=InvaderYB.HEIGHT;
 		new LwjglApplication(new InvaderYB(), config);
 	}
 }
