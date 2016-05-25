495d3db93c2313f2cc21f1b31e780d15631e51c2
Dave Arneson <urvaius@gmail.com>
2016-05-25 14:38:34 -0700

    base files

---
+++ desktop/src/com/arne5/invaderyb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.arne5.invaderyb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arne5.invaderyb.InvaderYB;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InvaderYB(), config);
+	}
+}
